3f1760ac9243963102402dc988aece0d94f87746
techprograms <tech.gpr@gmail.com>
2024-10-08 11:17:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import { useReducer } from "react";
+import TicketForm from "./components/TicketForm";
+import ticketReducer from "./reducers/tickerReducer";
+import TicketList from "./components/TicketList";
+import { sortTickets } from "./utilities/sortingUtilities";
 
 function App() {
+  const initialState = {
+    tickets: [],
+    editingTicket: null,
+    sortPreference: "High to Low",
+  };
+
+  const [state, dispatch] = useReducer(ticketReducer, initialState);
+
+  const sortedTickets = sortTickets(state.tickets, state.sortPreference);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Bug Management System</h1>
+        <TicketForm
+          dispatch={dispatch}
+          editingTicket={state.editingTicket}
+        ></TicketForm>
+        {state.tickets.length > 0 && (
+          <div className="results">
+            <h2>All Tickets</h2>
+
+            <select
+              value={state.sortPreference}
+              onChange={(e) =>
+                dispatch({ type: "SET_SORTING", payload: e.target.value })
+              }
+            >
+              <option value={"High to Low"}>High to Low</option>
+              <option value={"Low to High"}>Low to High</option>
+            </select>
+
+            <TicketList
+              // tickets={state.tickets}
+              tickets={sortedTickets}
+              dispatch={dispatch}
+            ></TicketList>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
